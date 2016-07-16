d41d67fd90dcc0c66a05df742a48b9c2021aa0ca
Adam Boxall <adam.boxall@gmail.com>
2016-07-16 03:37:56 +0100

    Removed some bad files

--- test.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-
-fs.open('./test.txt', 'r+', function(err, fd) {
-  fs.write(fd, 'foo', 4, function(err, written, string) {
-    console.log(err, written, string);
-  });
-});
